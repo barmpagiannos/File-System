473d1555c66b50339c15568932fe939a39b3ed5a
Vasilis Barmpagiannos <vasilismparmp@hotmail.gr>
2023-09-18 16:59:25 +0300

    filesystem.c

---
+++ filesystem.c
@@ -0,0 +1,15 @@
+#include "fileutil.h"
+
+// All the libraries the program needs to work properly are included to "fileutil.h" header file.
+// This is just a demonstration of how the program works.
+
+int main(){
+
+    write_users();
+    write_users();
+    read();
+    clear();
+
+    return 0;
+}
+
